63fb85f20ea04cdb508a9334482ceffe3fae272f
Elvin Li <elvinlkw@hotmail.com>
2025-03-09 23:33:39 -0400

    prettier change

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,7 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config.js";
-import {
-  questionsRoute,
-  userAnswersRoute,
-} from "./src/routes/index.js";
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config.js';
+import { questionsRoute, userAnswersRoute } from './src/routes/index.js';
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -14,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 // routes
-app.use("/questions", questionsRoute);
-app.use("/user-answers", userAnswersRoute);
+app.use('/api/questions', questionsRoute);
+app.use('/api/user-answers', userAnswersRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
