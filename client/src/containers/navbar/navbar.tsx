b0cb60d34f69ba97cadf845aa902799dc1ec7702
Elvin Li <elvinlkw@hotmail.com>
2025-03-11 00:15:51 -0400

    updated navbar

--- client/src/containers/navbar/navbar.tsx
+++ client/src/containers/navbar/navbar.tsx
@@ -1,22 +1,63 @@
-import { Button, Stack } from '@mui/material';
-import { NavLink, useNavigate } from 'react-router';
+import * as React from 'react';
+import { Link, NavLink, useNavigate } from 'react-router';
+import AdbIcon from '@mui/icons-material/Adb';
+import AppBar from '@mui/material/AppBar';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 import Cookies from 'js-cookie';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import { PATHS } from '../../routing';
+import Toolbar from '@mui/material/Toolbar';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
 import styled from '@emotion/styled';
 
-const NavContainer = styled.div({
-  backgroundColor: '#161616',
-  boxSizing: 'border-box',
-  padding: '12px',
-  width: '100svw',
-  margin: '0 auto',
+const pages = [
+  {
+    name: 'Home',
+    url: PATHS.LANDING_PAGE,
+  },
+  {
+    name: 'Admin',
+    url: PATHS.ADMIN_PAGE,
+  },
+];
+
+const LogoLink = styled(Link)({
   display: 'flex',
-  justifyContent: 'flex-end',
-  gap: '16px',
+  alignItems: 'center',
+  textDecoration: 'none',
+  color: 'white',
 });
 
 export const Navbar = () => {
   const navigate = useNavigate();
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   const handleLogOut = () => {
     Cookies.remove('jwttoken', { path: '/' });
@@ -24,23 +65,133 @@ export const Navbar = () => {
   };
 
   return (
-    <NavContainer>
-      <Stack spacing={2} direction="row">
-        <NavLink to={PATHS.LANDING_PAGE}>
-          {({ isActive }) => (
-            <Button variant={isActive ? 'contained' : 'outlined'}>Home</Button>
-          )}
-        </NavLink>
-        <NavLink to={PATHS.ADMIN_PAGE}>
-          {({ isActive }) => (
-            <Button variant={isActive ? 'contained' : 'outlined'}>Admin</Button>
-          )}
-        </NavLink>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <LogoLink to={PATHS.LANDING_PAGE}>
+            <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="span"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              E&M
+            </Typography>
+          </LogoLink>
 
-        <Button onClick={handleLogOut} variant={'outlined'}>
-          Log out
-        </Button>
-      </Stack>
-    </NavContainer>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+              {pages.map((page, idx) => (
+                <MenuItem key={idx} onClick={handleCloseNavMenu}>
+                  <Link to={page.url}>
+                    <Typography sx={{ textAlign: 'center' }}>
+                      {page.name}
+                    </Typography>
+                  </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="span"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            <LogoLink to={PATHS.LANDING_PAGE}>E&M</LogoLink>
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page, idx) => (
+              <NavLink to={page.url} key={idx}>
+                {({ isActive }) => (
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    sx={{
+                      my: 2,
+                      color: 'white',
+                      display: 'block',
+                      textDecoration: isActive ? 'underline' : 'none',
+
+                      ':hover': {
+                        textDecoration: 'underline',
+                      },
+                    }}
+                  >
+                    {page.name}
+                  </Button>
+                )}
+              </NavLink>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Admin" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <MenuItem onClick={handleLogOut}>
+                <Typography sx={{ textAlign: 'center' }}>Logout</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
