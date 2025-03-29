a12cd661dcdeec4d9205483807e1c1484756daee
Elvin Li <elvinlkw@hotmail.com>
2025-03-29 00:31:56 -0400

    added security

--- client/src/pages/features.tsx
+++ client/src/pages/features.tsx
@@ -1,14 +1,26 @@
 import { Box, Container, Switch, Typography } from '@mui/material';
+import { FEATURE_PLAY_GAME } from '../features';
 import { Feature } from '../api/services/features.service';
 import { useFeatureStore } from '../store/featureStore';
+import { useMemo } from 'react';
+import { useQuestions } from '../api/services/question';
 import { useToggleFeature } from '../api/hooks/useFeatures';
 
 export const FeaturesPage = () => {
   const { features } = useFeatureStore();
+  const { data: questions } = useQuestions();
+
+  const hasRevealedAnswers = useMemo(() => {
+    return questions?.some((q) => q.isAnswerRevealed);
+  }, [questions]);
 
   const { mutateAsync: toggleFeature } = useToggleFeature();
 
   const handleFeatureToggle = async (feature: Feature) => {
+    if (feature.featureKey === FEATURE_PLAY_GAME && hasRevealedAnswers) {
+      alert('All answers must be unrevealed to enable the game');
+    }
+
     await toggleFeature({
       featureId: feature.featureId,
       isEnabled: !feature.isEnabled,
