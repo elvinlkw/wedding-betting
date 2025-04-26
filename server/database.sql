b855b2d26131a8090043cfca09b7194b0f879385
Elvin Li <elvinlkw@hotmail.com>
2025-04-26 12:36:30 -0400

    added schemas

---
+++ server/database.sql
@@ -0,0 +1,65 @@
+CREATE DATABASE weddingbetting;
+
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT current_timestamp,
+  updated_at TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE questions (
+  question_id SERIAL PRIMARY KEY,
+  question_text VARCHAR(255),
+  question_text_fr VARCHAR(255),
+  is_enabled BOOLEAN DEFAULT TRUE,
+  is_answer_revealed BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT current_timestamp,
+  updated_at TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE question_choices (
+  choice_id SERIAL PRIMARY KEY,
+  question_id INT REFERENCES questions(question_id) ON DELETE CASCADE,
+  is_right_answer BOOLEAN DEFAULT FALSE,
+  choice_text VARCHAR(255) NOT NULL,
+  choice_text_fr VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT current_timestamp,
+  updated_at TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE user_answers (
+  answer_id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+  question_id INT REFERENCES questions(question_id) ON DELETE CASCADE,
+  choice_id INT REFERENCES question_choices(choice_id) ON DELETE CASCADE,
+  created_at TIMESTAMP DEFAULT current_timestamp,
+  updated_at TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE admin_user (
+  admin_id SERIAL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT current_timestamp,
+  updated_at TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE features (
+  feature_id SERIAL PRIMARY KEY,
+  feature_key VARCHAR(255) NOT NULL UNIQUE,
+  feature_name VARCHAR(255) NOT NULL,
+  feature_description VARCHAR(255),
+  is_enabled BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT current_timestamp,
+  updated_at TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE guests (
+  guest_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  table_number SMALLINT,
+  created_at TIMESTAMP DEFAULT current_timestamp,
+  updated_at TIMESTAMP DEFAULT current_timestamp
+);
